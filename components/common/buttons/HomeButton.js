be884cd527d927b9652047c89f6ae5ad27f6f79e
Jiwon Chae <63784453+jiwon0226@users.noreply.github.com>
2025-04-24 09:31:55 +0900

    feat: update HomeButton.js 헨들러 변경

--- components/common/buttons/HomeButton.js
+++ components/common/buttons/HomeButton.js
@@ -2,8 +2,8 @@ import { TouchableOpacity, StyleSheet} from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons'; // 아이콘 라이브러리
 import { styles } from './styles/HomeButton.styles';
 
-const HomeButton = ({ onPress }) => (
-  <TouchableOpacity style={styles.homeButton} onPress={onPress} activeOpacity={0.8}>
+const HomeButton = ({ onPressHandler }) => (
+  <TouchableOpacity style={styles.homeButton} onPress={onPressHandler} activeOpacity={0.8}>
     <Ionicons name="home" size={24} color="#385E3C" style={styles.icon}/>
   </TouchableOpacity>
 );
