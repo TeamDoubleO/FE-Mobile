5a633368706fa1a0598543ef70d1f377ceb2de6d
zzzzzisu <84431156+js4939@users.noreply.github.com>
2025-04-28 20:52:27 +0900

    Update NormalList.js
    - 실수로 삭제한 코드 복구하였습니다.

--- components/common/lists/NormalList.js
+++ components/common/lists/NormalList.js
@@ -24,7 +24,7 @@ const NormalList = ({ items, nextPage, style }) => {
         <TouchableOpacity
           key={index}
           onPress={() => handleSelect(index)}
-          style={[styles.itemBox, selectedIndex === index && styles.selectedItemBox]}
+          style={[styles.itemBox, selectedIndex === index && styles.selectedItemBox, style]}
         >
           <Text style={[styles.itemText, selectedIndex === index && styles.selectedItemText]}>
             {item}
